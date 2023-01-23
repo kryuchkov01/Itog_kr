6f713ec94594658f061a6fe2fdeb4b2d949b5196
Beliatskaya <beliatskaya1003@gmail.com>
2023-01-23 03:58:27 +0300

    dz_6

---
+++ Documents/что-то лоховское/dz_6/task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число : ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+Console.WriteLine("Введите размер массива: ");
+int size =  Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [size];
+array = FillArray(array);
+Console.WriteLine("Ваш массив : ");
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count ++;
+        }
+    }
+
+Console.WriteLine(count);
+
