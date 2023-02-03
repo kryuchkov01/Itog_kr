5e96b6ad424a3833f02b38db4cf11b936481fef3
Beliatskaya <beliatskaya1003@gmail.com>
2023-02-03 11:43:07 +0300

    dz_9

---
+++ Documents/что-то лоховское/dz_9/task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в 
+// промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int PrintNumbers(int number1, int number2)
+{
+    if(number1 == number2)
+    {
+        return number1;
+    }
+    else Console.Write(PrintNumbers(number1, number2+1) + " ");
+    return number2;
+}
+
+
+Console.WriteLine("Введите число: ");
+int n =  Convert.ToInt32(Console.ReadLine());
+int m = 0;
+PrintNumbers(n, m);
---
+++ Documents/что-то лоховское/dz_9/task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в 
+// промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int M, int N)
+{
+    int sum = 0;
+    if(M-1 == N)
+    {
+        return sum;
+    }
+    return sum = N + SumNumbers(M, N - 1);
+}
+
+
+Console.WriteLine("Введите числа M и N: ");
+int M =  Convert.ToInt32(Console.ReadLine());
+int N =  Convert.ToInt32(Console.ReadLine());
+int result = SumNumbers(M, N);
+Console.WriteLine(result);
+
